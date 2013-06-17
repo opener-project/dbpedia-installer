851bd0a07adc58a8606cfe1890361c9c739a0f7b
Yorick Peterse <yorickpeterse@olery.com>
2013-06-17 14:31:07 +0200

    Refactored the installer.
    Currently the installer fails as for some reason running `mvn clean install` in
    the dbpedia-spotlight directory results in Maven dependency errors.
    
    Signed-off-by: Yorick Peterse <yorickpeterse@olery.com>

--- core/install.bash
+++ core/install.bash
@@ -1,74 +1,114 @@
 #!/usr/bin/env bash
 
-LANG=$1
-INDEX=$2
-SPOTLIGHTDIR="dbpedia-spotlight";
+set -e
 
-cd ../../
+##
+# Enters the directory given in the first argument and notifies the user about
+# this.
+#
+# @param [String] $1 The directory to change the current working directory to.
+#
+enter_directory()
+{
+    full_path=$(readlink -f $1)
 
-if [ $# -eq 0 ] || [ $# -eq 1 ]
-  then
-  echo "Not enough arguments supplied; Please supply -lang and index-lang.tgz path as arguments.
-        For example: ./install.bash es /home/user/index-es.tgz"
-  exit 1
-fi
+    echo "Changing PWD to ${full_path}"
+    cd $1
+}
 
-if [ -d $SPOTLIGHTDIR ]
+##
+# Shows the message passed as the first argument and terminates the script.
+#
+# @param [String] $1 The message to display.
+#
+abort()
+{
+    echo $1
+    exit 1
+}
+
+##
+# Copies over a file and backs up existing files. If a backup already exists
+# this function doesn't overwrite it again.
+#
+# @param [String] $1 The source file.
+# @param [String] $2 The destination file.
+#
+copy_file()
+{
+    if [[ ! -f "$2.orig" ]]
+    then
+        cp --backup --suffix=.orig -f $1 $2
+    else
+        "Backup file for ${2} already exists, ignoring"
+    fi
+}
+
+language=$1
+index=$2
+script_dir=$(dirname $(readlink -f $0))
+dbpedia_git="https://github.com/dbpedia-spotlight/dbpedia-spotlight.git"
+dbpedia_dir="${script_dir}/dbpedia-spotlight"
+old_pwd=$(pwd)
+
+if [[ -z "${language}" ]]
 then
-    echo "dbpedia spotlight already exists skipping the step of downloading it";
-else
-    echo "downloading dbpedia spotlight";
-    git clone https://github.com/dbpedia-spotlight/dbpedia-spotlight.git
+    abort "You must specify a language as the first argument"
 fi
 
-cd $SPOTLIGHTDIR
-
-if [ -e pom.xml.orig ]
+if [[ -z "${index}" ]]
 then
-    echo "the original pom.xml files have been already replaced, skipping the replacement"
-    cd ..
-else
-    mv pom.xml pom.xml.orig
-    mv core/pom.xml core/pom.xml.orig
-    cd ../EHU-DBpedia-Spotlight/core
-    cp pom.xml ../../dbpedia-spotlight/
-    cp core/pom.xml ../../dbpedia-spotlight/core/
-    cp conf/server_$LANG.properties ../../dbpedia-spotlight/conf/
-    cd ../../
+    abort "You must specify an index file as the second argument"
 fi
 
-cd $SPOTLIGHTDIR
-echo "installing the modified dbpedia spotlight"
+if [[ ! -f $index ]]
+then
+    abort "The index file ${index} does not exist"
+# Expand the (potential) relative path to the full path so we can be sure that
+# later on the right file is used.
+else
+    index=$(readlink -f $index)
+    index_name=$(basename $index)
+fi
+
+if [[ ! -d $dbpedia_dir ]]
+then
+    echo 'dbpedia-spotlight directory does not exist, creating...'
+
+    if ! hash git 2>/dev/null
+    then
+        abort 'Git (http://git-scm.com/) is not installed, aborting...'
+    fi
+
+    git clone $dbpedia_git $dbpedia_dir
+fi
+
+echo 'Copying Maven configuration files to the dbpedia-spotlight directory...'
+
+copy_file "${script_dir}/pom.xml" $dbpedia_dir
+copy_file "${script_dir}/core/pom.xml" "${dbpedia_dir}/core"
+
+copy_file "${script_dir}/conf/server_${language}.properties" \
+    "${dbpedia_dir}/conf"
+
+echo 'Installing dependencies for all Maven projects...'
+enter_directory $dbpedia_dir
 mvn clean install
 
-echo "creating the jar with dependencies"
-cd dist
+echo 'Creating dbpedia-spotlight JAR archive...'
+enter_directory dist
 mvn clean package
 
-echo "creating a directory to store the indexes..."
-cd ..
+echo 'Creating directory for the indexes...'
+enter_directory ..
+mkdir -p data
 
-if [ -d data ]
-then
-    echo "moving to data directory"
-else
-    mkdir data
-    echo "making data directory"
-fi
+echo 'Preparing indexes...'
+enter_directory data
+cp -f $index .
+tar -xvf $index_name
+rm $index_name
 
-cd data
+enter_directory $old_pwd
 
-if [ -e index-$LANG.tgz ]
-
-then
-    echo "unzipping index ..."
-    tar xzvf index-$LANG.tgz
-    echo "DONE"
-else
-    echo "Copying index..."
-    cp $INDEX .
-    tar xvzf index-$LANG.tgz
-    echo "DONE"
-fi
-
-echo "Installation completed."
+echo 'Finished installing'
